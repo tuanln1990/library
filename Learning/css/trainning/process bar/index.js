aae7a937ff71642c51351ae5844c844b82e1c29b
Tuan Le <tuanln1990@gmail.com>
2020-02-17 17:45:29 +0700

    add how to get style css in javascript and add preload page

--- Learning/css/trainning/process bar/index.js
+++ Learning/css/trainning/process bar/index.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded',function() {
-	
+	var prc = document.querySelector('span');
+	var styleofPrc = getComputedStyle(prc);
+	var number = document.querySelector('label');
+	var widthofSpan = styleofPrc.getPropertyValue("width").substr(0, styleofPrc.getPropertyValue("width").length - 2);
+	var styleofLine = getComputedStyle(document.querySelector('.line'));
+	var widthofLine = styleofLine.getPropertyValue('width').substr(0,styleofLine.getPropertyValue('width').length - 2);
+	var percent = widthofSpan/widthofLine * 100;
+	// var prc = document.querySelector('span');
+	// var number = document.querySelector('label');
+	// var percent = prc.style.width.substring(0,prc.style.width.length-1);
+	// console.log(percent);
+	var i = 0;
+	setInterval(function(){
+		if(i < JSON.parse(percent))
+		{
+			i++;
+			number.textContent = i + '%';
+			prc.style.width = i + '%';
+		}
+	},15);
 });
